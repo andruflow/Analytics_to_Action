73bcb577bbd6d9eec175528eeadae4877dfb85e3
Julien Hirsch <91834763+hirschjulien@users.noreply.github.com>
2025-03-10 14:45:07 +0100

    Update clean_data_solar.py

--- clean_data_solar.py
+++ clean_data_solar.py
@@ -1,13 +1,19 @@
 import pandas as pd
-import numpy as np
 
 df_data = pd.read_excel("Global-Solar-Power-Tracker-February-2025.xlsx", sheet_name="20 MW+")
 df_threshold = pd.read_excel("Global-Solar-Power-Tracker-February-2025.xlsx", sheet_name="1-20 MW")
 
 df = df_data.append(df_threshold)
 
-selected_columns = ['Country/Area', 'Capacity (MW)', 'Technology Type', 'Start year', 'Retired year', 'Owner', 'Latitude', 'Longitude']
+selected_columns = ['Country/Area', 'Capacity (MW)', 'Technology Type', 'Status', 'Start year', 'Retired year', 'Owner', 'Latitude', 'Longitude']
 df = df[selected_columns]
+
+# calculate and add 'Lifetime' column
+df['Lifetime'] = pd.to_numeric(df['Retired year'], errors='coerce') - pd.to_numeric(df['Start year'], errors='coerce')
+
+# turn 'Owner' into lower case and replace empty tiles with 'N/A'
+df['Owner'] = df['Owner'].str.lower()
 df = df.applymap(lambda x: "N/A" if pd.isna(x) else x)
+df['Lifetime'] = df['Lifetime'].apply(lambda x: "X" if x == "N/A" else x)
 
 df.to_excel('solar_cleaned.xlsx', index=False)
